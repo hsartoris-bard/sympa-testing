6f093ce3d4dfdd8d433e45e0169bab22527caffc
Hayden Sartoris <hsartoris@bard.edu>
2018-12-05 11:30:24 -0500

    more stuff

--- migrate.sh
+++ migrate.sh
@@ -11,18 +11,24 @@ DBDUMP="/home/smaguire/sympa02_db_dump_20181107.sql.gz"
 FROMDIR="/mnt/sympa02_files/"
 TODIR="/var/lib/sympa/"
 
+echo "----Current settings----"
+echo "Loading database from: ${DBDUMP}"
+echo "Copying lists and archives from: ${FROMDIR}"
+echo "Copying lists and archives to: ${TODIR}"
+
 echo "WARNING: proceeding beyond this point will remove ALL Sympa entries from MySQL, list_data, and arc"
 while true; do
-	read -p "Continue? [yes/no]" yn
+	read -p "Continue? [yes/*] " yn
 	case $yn in
 		yes ) break;;
 		* ) exit;;
 	esac
 done
 
-echo "got here"
-exit
+echo "Deleting old database"
 
+echo mysql -u root -p$SQLROOT -e "DROP DATABASE sympa;"
+exit 
 sudo rm -r /var/lib/sympa/list_data/sympa.bard.edu/*
 sudo rm -r /var/lib/sympa/arc/*
 
