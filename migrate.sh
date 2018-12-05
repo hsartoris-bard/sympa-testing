a74ffe7c2a57ad1aa9b07318913ad9cc278e29cd
Hayden Sartoris <hsartoris@bard.edu>
2018-12-05 11:23:36 -0500

    more scripting

--- migrate.sh
+++ migrate.sh
@@ -5,13 +5,23 @@ echo " "
 read -s -p "SQL sympa password:" SQLSYMPA
 echo " "
 SQLSTRING="CREATE DATABASE sympa CHARACTER SET utf8; GRANT ALL PRIVILEGES ON sympa.* to sympa@localhost IDENTIFIED BY ${SQLSYMPA};"
-echo $SQLSTRING
-exit
 
 DBDUMP="/home/smaguire/sympa02_db_dump_20181107.sql.gz"
 
 FROMDIR="/mnt/sympa02_files/"
 TODIR="/var/lib/sympa/"
+
+echo "WARNING: proceeding beyond this point will remove ALL Sympa entries from MySQL, list_data, and arc"
+while true; do
+	read -p "Continue? [yes/no]" yn
+	case $yn in
+		yes ) break;;
+		* ) exit;;
+	esac
+done
+
+echo "got here"
+exit
 
 sudo rm -r /var/lib/sympa/list_data/sympa.bard.edu/*
 sudo rm -r /var/lib/sympa/arc/*
