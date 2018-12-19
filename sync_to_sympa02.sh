cfab0f37b62db03c6110dbe82bd95c42e94b839c
Hayden Sartoris <hsartoris@bard.edu>
2018-12-19 10:39:31 -0500

    moved in blacklist files

--- sync_to_sympa02.sh
+++ sync_to_sympa02.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-rsync -az --itemize-changes --exclude-from=/home/hsartoris/blacklist_arc root@sympa02.bard.edu:/usr/local/sympa/arc/ /var/lib/sympa/arc/
+rsync -az --itemize-changes --exclude-from=/home/hsartoris/sympa-testing/blacklist_arc root@sympa02.bard.edu:/usr/local/sympa/arc/ /var/lib/sympa/arc/
 
-rsync -az --itemize-changes --exclude-from=/home/hsartoris/blacklist_list_data root@sympa02.bard.edu:/usr/local/sympa/list_data/sympa.bard.edu/ /var/lib/sympa/list_data/sympa.bard.edu/
+rsync -az --itemize-changes --exclude-from=/home/hsartoris/sympa-testing/blacklist_list_data root@sympa02.bard.edu:/usr/local/sympa/list_data/sympa.bard.edu/ /var/lib/sympa/list_data/sympa.bard.edu/
