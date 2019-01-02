1504e1d7fad3fffcfa6154da09d85052f7e4e933
Hayden Sartoris <hsartoris@bard.edu>
2019-01-02 13:45:52 -0500

    added spool sync

--- sync_to_sympa02.sh
+++ sync_to_sympa02.sh
@@ -3,3 +3,5 @@
 rsync -az --itemize-changes --exclude-from=/home/hsartoris/sympa-testing/blacklist_arc root@sympa02.bard.edu:/usr/local/sympa/arc/ /var/lib/sympa/arc/
 
 rsync -az --itemize-changes --exclude-from=/home/hsartoris/sympa-testing/blacklist_list_data root@sympa02.bard.edu:/usr/local/sympa/list_data/sympa.bard.edu/ /var/lib/sympa/list_data/sympa.bard.edu/
+
+rsync -az --itemize-changes root@sympa02.bard.edu:/usr/local/sympa/spool/ /var/spool/sympa/
