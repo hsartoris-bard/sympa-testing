23e1cf5f772a49cf9778237d6a93c0ea0d1537c3
Hayden Sartoris <hsartoris@bard.edu>
2019-01-08 16:07:15 -0500

    minor changes

--- rsync_then_migrate.sh
+++ rsync_then_migrate.sh
@@ -1,17 +1,20 @@
 #!/bin/bash
 
+
+echo STOP! have you copied the database?
+echo "mysqldump -u sympa -p --databases sympa > sympa02.sql"
+echo check the sympa02 conf for pass
+exit
+
 echo Rsyncing
 
 systemctl stop sympa wwsympa 
 
 /home/hsartoris/sympa-testing/sync_to_sympa02.sh
 
-echo Still needs to import db, check bounce storage
-exit
+#sympa.pl --upgrade --from=6.1.19 --to=6.2.37b.2
+sympa.pl --upgrade --from=6.1.19 --to=6.2.38
 
-sympa.pl --upgrade --from=6.1.19 --to=6.2.37b.2
-
-systemctl start sympa wwsympa postfix 
 
 echo Adding aliases
 
@@ -22,7 +25,7 @@ do
 done
 
 echo Reloading postfix
-
+systemctl start sympa wwsympa postfix 
 postfix reload
 
 echo Done
